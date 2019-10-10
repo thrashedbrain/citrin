881e1286e0f0c614d5fa212eddd7b53ad42f085a
Юрий <yakura12kk@gmail.com>
2019-10-10 07:10:50 +0700

    workers

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+from bson import ObjectId
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
@@ -7,24 +8,59 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/citrin_caller"
 mongo = PyMongo(app)
 
+currentMail = ""
+currentPassword = ""
 
-@app.route('/workers')
+
+@app.route('/workers/')
 def workers():
     workers = mongo.db.workers.find({})
     datastr = []
     for document in workers:
-        datastr.append({'name': document['name']})
+        datastr.append({'name': document['name'],
+                        'username': document['username'],
+                        'password': document['password'],
+                        'id': str(document['_id'])})
 
     return jsonify(datastr)
 
 
-@app.route('/setworkers', methods=['POST'])
-def setworkers():
+@app.route('/setworkers/', methods=['POST'])
+def addworkers():
+    if request.form.get("name") == "" \
+            or request.form.get("name") is None \
+            or request.form.get("username") == "" \
+            or request.form.get("username") is None \
+            or request.form.get("password") == "" \
+            or request.form.get("password") is None:
+        return jsonify({"Status": "Err"})
+
     data = {
-        "name": request.form.get('name')
+        "name": request.form.get('name'),
+        "username": request.form.get('username'),
+        "password": request.form.get('password')
     }
     collection = mongo.db.workers.insert_one(data)
-    return jsonify("asd")
+    return jsonify({"Status": "Added"})
+
+
+@app.route('/setworker/', methods=['POST'])
+def setworker():
+    id = request.form.get("id")
+    print(id)
+    collection = mongo.db.workers.find_one({'_id': ObjectId(id)})
+
+    if collection['username'] is not None \
+            and collection['username'] != "" \
+            and collection['password'] is not None \
+            and collection['username'] != "":
+        currentMail = collection['username']
+        currentPassword = collection['password']
+        print(currentMail)
+        return jsonify({"Status": "Ok"})
+
+    else:
+        return jsonify({"Status": "Err"})
 
 
 @app.route('/blacklist/', methods=['POST', 'GET', 'DELETE'])
@@ -84,8 +120,8 @@ def setlead():
                         }
                     })
                     req = requests.post('https://citrin.bitrix24.ru/crm/configs/import/lead.php', data={
-                        'LOGIN': "citrin@citrin.su",
-                        'PASSWORD': "PlayBoy69",
+                        'LOGIN': currentMail,
+                        'PASSWORD': currentPassword,
                         'TITLE': "Авито",
                         'PHONE_MOBILE': phone
                     })
